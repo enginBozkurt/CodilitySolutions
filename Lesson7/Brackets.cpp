e6f4db6dd46c596457dc8fb5b42014b73c17225c
Engin Bozkurt <30608533+enginBozkurt@users.noreply.github.com>
2019-08-20 17:29:07 +0300

    Stacks and Queues

---
+++ Lesson7/Brackets.cpp
@@ -0,0 +1,21 @@
+#include <stack>  
+  
+int solution(string &S) {  
+	  
+	  
+	// write your code in C++14 (g++ 6.2.0)  
+	stack <char> open; // stack of opening brackets  
+	  
+	  
+	for (auto &s: S)  
+		if (s == '(' or s == '[' or s == '{') open.push (s);   // opening bracket -> add to stack  
+		else if (open.size() == 0) return false;        // closing bracket before opening -> badly nested  
+		else if (s == ')' and open.top() != '(') return false; // closing bracket does not match last opened bracket  
+		else if (s == ']' and open.top() != '[') return false; // so badly nested  
+		else if (s == '}' and open.top() != '{') return false;  
+		  
+		else open.pop(); // bracket closed, remove from stack  
+  
+	return open.size() == 0; // check if some opened brackets left  
+  
+}  
